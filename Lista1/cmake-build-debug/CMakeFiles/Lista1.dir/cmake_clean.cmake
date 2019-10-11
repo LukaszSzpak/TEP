d205aec8b4ed965cb64e41c223499cd01f77ad47
LukaszSzpak <50335669+LukaszSzpak@users.noreply.github.com>
2019-10-11 21:14:27 +0200

    Utworzenie projektu listy 1

---
+++ Lista1/cmake-build-debug/CMakeFiles/Lista1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lista1.dir/main.cpp.obj"
+  "Lista1.exe"
+  "Lista1.exe.manifest"
+  "Lista1.pdb"
+  "libLista1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lista1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
