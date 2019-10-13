88a7a4872f203c1842dbe8973b397d48377e6f7e
LukaszSzpak <50335669+LukaszSzpak@users.noreply.github.com>
2019-10-14 00:10:26 +0200

    polecenie 1 + 2

--- Lista1/main.cpp
+++ Lista1/main.cpp
@@ -1,6 +1,65 @@
 #include <iostream>
 
+
+//polecenie 1
+void v_alloc_table_add_5(int iSize) {
+    std::cout<<"Zadanie 1:\n";
+    if(iSize < 0) {
+        std::cout<<"Niepoprawne wywołanie funkcji !";
+        return;
+    }
+    //tworzenie tablicy
+    int *tab;
+    tab = new int[iSize];
+
+    // uzupełnienie tablicy
+    for (int i = 0; i < iSize; i++) {
+        tab[i] = i + 5;
+    }
+
+    //wyswietlenie tablicy
+    for(int i = 0; i < iSize; i++) {
+        std::cout<<tab[i]<<" ";
+    }
+    std::cout<<"\n";
+
+    //usuwane tablicy
+    delete(tab);
+}
+
+//polecenie 2
+bool b_alloc_table_2_dim(int **(*piTable), int iSizeX, int iSizeY) {
+    if (iSizeX <=0 || iSizeY <= 0)
+        return false;
+
+    *piTable = new int*[iSizeX];
+    for (int i = 0; i < iSizeX; i++) {
+        (*piTable)[i] = new int[iSizeY];
+
+        for (int ii = 0; ii < iSizeY; ii++)
+            (*piTable)[i][ii] = i + ii;  //a->   (a*)
+    }
+
+    return true;
+}
+
+// funkcja wyswietalajaca tablice
+void print_tab(int **(*piTable), int iSizeX, int iSizeY) {
+    std::cout<<"\nWyswietlam tablice:\n";
+    for (int i = 0; i < iSizeX; i++) {
+        for (int j = 0; j < iSizeY; j++)
+            std::cout<<(*piTable)[i][j]<<" ";
+        std::cout<<"\n";
+    }
+}
+
+//testowy main
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    v_alloc_table_add_5(5);
+
+    int** tab;
+    b_alloc_table_2_dim(&tab, 3, 5);
+    print_tab(&tab, 3, 5);
+
     return 0;
 }
