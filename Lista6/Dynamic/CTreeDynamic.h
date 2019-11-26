ee807c5149ea7860fbc4bd78ebc0937268d9c6cb
LukaszSzpak <50335669+LukaszSzpak@users.noreply.github.com>
2019-11-26 12:24:42 +0100

    Lista 6 gotowa

--- Lista6/Dynamic/CTreeDynamic.h
+++ Lista6/Dynamic/CTreeDynamic.h
@@ -7,7 +7,7 @@
 
 #include "CNodeDynamic.h"
 
-template <typename T> class CTreeDynamic {
+template < typename T > class CTreeDynamic {
 private:
     CNodeDynamic<T> *pc_root;
 
@@ -17,8 +17,45 @@ public:
     CNodeDynamic<T> *pcGetRoot();
     void vPrintTree();
     bool bMoveSubtree(CNodeDynamic<T> *pcParentNode, CNodeDynamic<T> *pcNewChildNode);
+};//class CTreeDynamic
 
-};//class CTreeStatic
+    template <typename T>
+    CTreeDynamic<T>::CTreeDynamic() {
+        pc_root = new CNodeDynamic<T>();
+    }
+
+    template <typename T>
+    CTreeDynamic<T>::~CTreeDynamic() {
+        delete pc_root;
+    }
+
+    template <typename T>
+    void CTreeDynamic<T>::vPrintTree() {
+        pc_root->vPrintAllBelow();
+    }
+
+    template <typename T>
+    bool CTreeDynamic<T>::bMoveSubtree(CNodeDynamic<T> *pcParentNode, CNodeDynamic<T> *pcNewChildNode) {
+        if (pcParentNode != NULL && pcNewChildNode != NULL) {
+            if (pcParentNode->pcGetRoot(pcParentNode) == pcNewChildNode->pcGetRoot(pcNewChildNode))
+                return false;
+
+            CNodeDynamic<T> *pc_parent_temp = pcNewChildNode->pcGetParent();
+            if (pc_parent_temp == NULL)
+                return false;
+
+            pcParentNode->vAddNewChild(pcNewChildNode);
+            pc_parent_temp->vRemoveChild(pcNewChildNode);
+            return true;
+        }
+
+        return false;
+    }
+
+    template <typename T>
+    CNodeDynamic<T> *CTreeDynamic<T>::pcGetRoot() {
+        return pc_root;
+    }
 
 
 #endif //LISTA5_CTREEDYNAMIC_H
--- Lista6/main.cpp
+++ Lista6/main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 #include "Dynamic/CTreeDynamic.h"
+#include <string>
 
 int main() {
 
-    CTreeDynamic<int> *c_tree = new CTreeDynamic<int>();
-    CNodeDynamic<int> *c_root = c_tree->pcGetRoot();
+    CTreeDynamic<std::string> *c_tree = new CTreeDynamic<std::string>();
+    CNodeDynamic<std::string> *c_root = c_tree->pcGetRoot();
     c_root->vAddNewChild();
     c_root->vAddNewChild();
-    c_root->pcGetChild(0)->vSetValue(1);
-    c_root->pcGetChild(1)->vSetValue(2);
+    c_root->pcGetChild(0)->vSetValue("1");
+    c_root->pcGetChild(1)->vSetValue("2");
     c_root->pcGetChild(0)->vAddNewChild();
     c_root->pcGetChild(0)->vAddNewChild();
-    c_root->pcGetChild(0)->pcGetChild(0)->vSetValue(11);
-    c_root->pcGetChild(0)->pcGetChild(1)->vSetValue(12);
+    c_root->pcGetChild(0)->pcGetChild(0)->vSetValue("11");
+    c_root->pcGetChild(0)->pcGetChild(1)->vSetValue("12");
     c_root->pcGetChild(1)->vAddNewChild();
     c_root->pcGetChild(1)->vAddNewChild();
-    c_root->pcGetChild(1)->pcGetChild(0)->vSetValue(21);
-    c_root->pcGetChild(1)->pcGetChild(1)->vSetValue(22);
+    c_root->pcGetChild(1)->pcGetChild(0)->vSetValue("21");
+    c_root->pcGetChild(1)->pcGetChild(1)->vSetValue("22");
     c_tree->vPrintTree();
 
     return 0;
